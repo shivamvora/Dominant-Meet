e5dc8afa4a87bebe6c3d829bcc1d2bbd192e8d69
shivamvora <shivampatel27102001@gmail.com>
2021-04-07 14:56:01 +0530

    routes

---
+++ src/components/CallPage/CallPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  './CallPage.scss';
+
+const CallPage = () => {
+    return (
+        <div>
+            <h1>CallPage</h1>
+        </div>
+    )
+}
+
+export default CallPage;
---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NoMatch.scss';
+
+const NoMatch = () => {
+    return (
+        <div>
+            <h1>NoMatch</h1>
+        </div>
+    )
+}
+
+export default NoMatch;
